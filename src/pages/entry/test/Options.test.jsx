deb15d91b36d84cd92a2b905600bd4317742b270
Pavlo Danis <danispavlolll@gmail.com>
2022-07-15 06:49:08 +0300

    Add images fetching test

---
+++ src/pages/entry/test/Options.test.jsx
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+
+import Options from "../Options";
+
+test("displays image for each scoop option from server", async () => {
+  render(<Options optionType="scoops" />);
+
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
+
+  expect(scoopImages).toHaveLength(4);
+
+  const altText = scoopImages.map((element) => element.alt);
+
+  expect(altText).toEqual([
+    "Mint chip scoop",
+    "Vanilla scoop",
+    "Chocolate scoop",
+    "Salted caramel scoop",
+  ]);
+});
